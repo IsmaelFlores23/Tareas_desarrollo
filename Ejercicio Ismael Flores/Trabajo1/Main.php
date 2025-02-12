c8c39c8d39a14a35eb8f4fcc94a41f801244a00f
Ismael Flores <ia_quiotof@unicah.edu>
2025-02-11 22:19:26 -0600

    haciendo segundo ejercicio

--- Ejercicio Ismael Flores/Trabajo1/Main.php
+++ Ejercicio Ismael Flores/Trabajo1/Main.php
@@ -22,7 +22,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trabajo de Bootstrap </title>
+    <title>Ejercicos de Bootstrap </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
@@ -30,16 +30,9 @@ try {
 
 <body>
 
-<div class="h4 pb-2 mb-4 text-danger border-bottom border-danger">
-    <h1>Peliculas Mas Rentadas</h1>
-</div>
+<h1>Peliculas Mas Rentadas</h1>
 
 
-
-
-<ul class="list-group">
-
-</ul>
 
 <ul class="list-group">
 
